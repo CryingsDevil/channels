4b09703e19a939a925c18884207f0b6e5743ecbd
CryingsDevil <550273382@qq.com>
2017-08-20 13:01:26 +0800

    All problem solved

--- channels/channels.c
+++ channels/channels.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "channels.h"
 
@@ -21,7 +22,7 @@ void *working(int *id){
 	int locked = 0;
 	while(1){
 		while(!locked)locked =	pthread_mutex_trylock(&channels[cid]->recv);
-		locked = 0;		
+		locked = 0;
 		while(!channels[cid]->hadMessage);
 		pthread_mutex_unlock(&channels[cid]->recv);
 		while(!locked)locked = pthread_mutex_trylock(&channels[cid]->message);
@@ -43,13 +44,14 @@ int ch_setup(){
 			pthread_mutex_init(&channels[i]->message, NULL);
 			pthread_mutex_init(&channels[i]->recv, NULL);
 			ret = pthread_create(&channels[i]->pid,NULL,(void *)working, &channels[i]->id);   
-
+			usleep(1000);
 			if(ret == 0){
 				ret = -1;
 				success++;		
 			}else{
 				puts("Create struct error");
 			}
+		
 		
 		}
 		
@@ -62,63 +64,85 @@ int ch_setup(){
 }
 
 int ch_send(int channel, void* msg){
-	int locked = 0;
-	while(!locked){
-		locked = pthread_mutex_trylock(&channels[channel]->message);
-		if(locked){
-			channels[channel]->item = (void*)malloc(sizeof((void*) msg));
-			channels[channel]->item = msg;
-			msg = NULL;
-			free(msg);		
-			channels[channel]->hadMessage = 1;
-			pthread_mutex_unlock(&channels[channel]->message);
+	if(channel >= 8){
+		puts("Error: Channel number is larger than 7!");
+		return -1;
+	}else{
+		int locked = 0;
+		while(!locked){
+			locked = pthread_mutex_trylock(&channels[channel]->message);
+			if(locked){
+				if(channels[channel]->hadMessage == 0){
+					channels[channel]->item = (void*)malloc(sizeof((void*) msg));
+					channels[channel]->item = msg;
+					msg = NULL;
+					free(msg);
+					channels[channel]->hadMessage = 1;
+					usleep(1000);
+				}else{
+					locked = 0;
+				}
+				pthread_mutex_unlock(&channels[channel]->message);
+				
+			}
+			usleep(1000);
 		}
+		return 0;
 	}
-	return 0;
-
 }
 
 int ch_recv(int channel, void** dest){
-	int locked = 0;
-	while(!locked){
-		locked = pthread_mutex_trylock(&channels[channel]->recv);
-		if(locked){
-			if(channels[channel]->hadMessage == 1){
-				*dest = (void*)malloc(sizeof((void*) channels[channel]->item)); 
-				*dest = channels[channel]->item;
-				channels[channel]->item = NULL;
-				free(channels[channel]->item;
-				channels[channel]->hadMessage = 0;
-			}else{
-				locked = 0;
+	if(channel >= 8){
+		puts("Error: Channel number is larger than 7!");
+		return -1;
+	}else{
+		int locked = 0;
+		while(!locked){
+
+			locked = pthread_mutex_trylock(&channels[channel]->recv);
+			if(locked){
+				if(channels[channel]->hadMessage == 1){
+					*dest = (void*)malloc(sizeof((void*) channels[channel]->item));
+					*dest = channels[channel]->item;
+					channels[channel]->item = NULL;
+					free(channels[channel]->item);
+					channels[channel]->hadMessage = 0;
+					usleep(1000);
+				}else{
+					locked = 0;
+				}
+				pthread_mutex_unlock(&channels[channel]->recv);
+
 			}
-			pthread_mutex_unlock(&channels[channel]->recv);
-
+			usleep(1000);
 		}
+		return 0;
 	}
-	return 0;
-
 }
 
 
 int ch_tryrecv(int channel, void** dest){
-	if(channels[channel]->hadMessage == 1){
-		if(pthread_mutex_trylock(&channels[channel]->recv) == 0){
-			*dest = channels[channel]->item;
-			channels[channel]->item = NULL;
-			free(channels[channel]->item);
-			channels[channel]->hadMessage = 0;
-			pthread_mutex_unlock(&channels[channel]->recv);
-			return 1;
+	if(channel >= 8){
+		puts("Error: Channel number is larger than 7!");
+		return -1;
+	}else{
+		if(channels[channel]->hadMessage == 1){
+			if(pthread_mutex_trylock(&channels[channel]->recv) == 0){
+				*dest = channels[channel]->item;
+				channels[channel]->item = NULL;
+				free(channels[channel]->item);
+				channels[channel]->hadMessage = 0;
+				pthread_mutex_unlock(&channels[channel]->recv);
+				return 1;
+			}else{
+				dest = NULL;
+				return 0;
+			}
 		}else{
 			dest = NULL;
 			return 0;
 		}
-	}else{
-		dest = NULL;
-		return 0;
 	}
-
 }
 
 
