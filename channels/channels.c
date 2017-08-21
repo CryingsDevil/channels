413a5a05710cc8d318608095565aa79270640fcd
CryingsDevil <550273382@qq.com>
2017-08-21 12:39:10 +0800

    Upgrated to pthread_cond_wait()

--- channels/channels.c
+++ channels/channels.c
@@ -9,7 +9,12 @@
 typedef struct{
 	int id;
 	int hadMessage;
-	pthread_mutex_t message;
+	pthread_cond_t condS; 
+	pthread_cond_t condR;
+	pthread_cond_t condCS; 
+	pthread_cond_t condCR;
+
+	pthread_mutex_t send;
 	pthread_mutex_t recv;
 	void* item;
 	pthread_t pid;
@@ -17,23 +22,60 @@ typedef struct{
 
 static channelL *channels[8];
 
-void *working(int *id){
-	int cid = *id;
-	int locked = 0;
-	while(1){
-		while(!locked)locked =	pthread_mutex_trylock(&channels[cid]->recv);
-		locked = 0;
-		while(!channels[cid]->hadMessage);
-		pthread_mutex_unlock(&channels[cid]->recv);
-		while(!locked)locked = pthread_mutex_trylock(&channels[cid]->message);
-		locked = 0;
-		while(channels[cid]->hadMessage);
-		pthread_mutex_unlock(&channels[cid]->message);
-	}
-}
+// void *working(int *id){
+// 	int cid = *id;
+// 	int locked = 0;
+// 	while(1){
+// 		puts("Channels trying to lock recv");
+// 		while(!locked)locked =	pthread_mutex_trylock(&channels[cid]->recv);
+// 		locked = 0;
+// 		puts("Channels lokced recv");
+// 		// while(channels[cid]->hadMessage == 0){
+// 		// 	puts("Calling send");
+// 		// 	pthread_cond_broadcast(&channels[cid]->condS);
+// 		// 	usleep(1000);
+// 		// }
+// 		pthread_cond_broadcast(&channels[cid]->condS);
+// 		puts("Channles broadcasted send");
+// 		// pthread_mutex_lock(&channels[cid]->recv);
+// 		while(channels[cid]->hadMessage == 0)pthread_cond_wait(&channels[cid]->condCS,&channels[cid]->recv);
+// 		puts("Channls get message");
+// 		// while(channels[cid]->hadMessage == 1){
+// 		// 	pthread_cond_broadcast(&channels[cid]->condR);
+// 		// 	usleep(1000);
+// 		// }
+// 		pthread_mutex_unlock(&channels[cid]->recv);
+
+// 		while(!locked)locked = pthread_mutex_trylock(&channels[cid]->send);
+// 		puts("Channles called recv");
+// 		locked = 0;
+// 		// pthread_mutex_lock(&channels[cid]->message);
+// 		// while(channels[cid]->hadMessage == 1){
+// 		// 	puts("Calling recv");
+// 		// 	pthread_cond_broadcast(&channels[cid]->condR);
+// 		// 	usleep(1000);
+// 		// }
+// 		pthread_cond_broadcast(&channels[cid]->condR);
+// 		puts("Channels waiting for recv finished");
+// 		while(channels[cid]->hadMessage == 1)pthread_cond_wait(&channels[cid]->condCR,&channels[cid]->send);
+// 		puts("Channles' message had been received");
+// 		pthread_mutex_unlock(&channels[cid]->send);
+
+
+		
+// 		// while(!locked)locked =	pthread_mutex_trylock(&channels[cid]->recv);
+// 		// locked = 0;
+// 		// while(!channels[cid]->hadMessage);
+// 		// pthread_mutex_unlock(&channels[cid]->recv);
+// 		// while(!locked)locked = pthread_mutex_trylock(&channels[cid]->message);
+// 		// locked = 0;
+// 		// while(channels[cid]->hadMessage);
+// 		// pthread_mutex_unlock(&channels[cid]->message);
+// 	}
+// }
 
 int ch_setup(){
-	int ret = -1;
+	int ret = 0;
 	int success = 0;
 	for(int i = 0; i <= 7; i++){
 		channels[i] = malloc(sizeof(channelL));
@@ -41,18 +83,24 @@ int ch_setup(){
 			channels[i]->id = i;
 			channels[i]->item = NULL;
 			channels[i]->hadMessage = 0;
-			pthread_mutex_init(&channels[i]->message, NULL);
-			pthread_mutex_init(&channels[i]->recv, NULL);
-			ret = pthread_create(&channels[i]->pid,NULL,(void *)working, &channels[i]->id);   
+			ret += pthread_cond_init(&channels[i]->condS , NULL) + 1;
+			ret += pthread_cond_init(&channels[i]->condR , NULL) + 1;
+			// pthread_cond_init(&channels[i]->condCS , NULL);
+			// pthread_cond_init(&channels[i]->condCR , NULL);
+			ret += pthread_mutex_init(&channels[i]->send , NULL) + 1;
+			ret += pthread_mutex_init(&channels[i]->recv , NULL) + 1;
+			// ret = pthread_create(&channels[i]->pid , NULL , (void *)working, &channels[i]->id);   
 			usleep(1000);
-			if(ret == 0){
-				ret = -1;
+			if(ret == 4	){
+				ret = 0;
 				success++;		
 			}else{
 				puts("Create struct error");
 			}
 		
 		
+		}else{
+			puts("No enough memory!");
 		}
 		
 	}
@@ -62,31 +110,77 @@ int ch_setup(){
 		return -1;
 	}
 }
+// void *producer(void *p)  
+// {  
+//     struct msg *mp;  
+//     while(1){  
+//        mp =  malloc(sizeof(struct msg));  
+//        mp->num = rand() % 1000 + 1;  
+//        printf("produce %d\n", mp->num);  
+       
+//        pthread_mutex_lock(&lock);  
+//        mp->next = head;  
+//        head = mp;         
+//        pthread_mutex_unlock(&lock);  
+//        pthread_cond_signal(&has_product);         
+//        sleep(2);  
+//     }  
+// }  
+  
+// void *consumer(void *p)  
+// {  
+//     while(1){  
+//         pthread_cond_wait(&has_product, &lock);  
+      
+//         struct msg *tmp = head;  
+//         while(tmp != NULL) {  
+//             printf("%d ", tmp->num);  
+//                 tmp = tmp->next;  
+//         }  
+//         printf("\n");  
+//     }  
+// }  
 
 int ch_send(int channel, void* msg){
 	if(channel >= 8){
 		puts("Error: Channel number is larger than 7!");
 		return -1;
 	}else{
-		int locked = 0;
-		while(!locked){
-			locked = pthread_mutex_trylock(&channels[channel]->message);
-			if(locked){
-				if(channels[channel]->hadMessage == 0){
-					channels[channel]->item = (void*)malloc(sizeof((void*) msg));
-					channels[channel]->item = msg;
-					msg = NULL;
-					free(msg);
-					channels[channel]->hadMessage = 1;
-					usleep(1000);
-				}else{
-					locked = 0;
-				}
-				pthread_mutex_unlock(&channels[channel]->message);
+		// usleep(1000);
+		// int locked = 0;
+		// while(!locked)locked = pthread_mutex_trylock(&channels[channel]->send);
+			// puts("Sends called");
+			pthread_mutex_lock(&channels[channel]->send);
+			while(channels[channel]->hadMessage == 1)pthread_cond_wait(&channels[channel]->condS,&channels[channel]->send);
+			// puts("Sends get singal");
+			// pthread_cond_wait(&channels[channel]->condS,&channels[channel]->message);
+			channels[channel]->item = (void*)malloc(sizeof((void*) msg));
+			channels[channel]->item = msg;
+			msg = NULL;
+			free(msg);
+			channels[channel]->hadMessage = 1;
+			pthread_cond_broadcast(&channels[channel]->condR);
+			pthread_mutex_unlock(&channels[channel]->send);
+			// puts("Sends finished!");
+					// int locked = 0;
+		// while(!locked){
+		// 	locked = pthread_mutex_trylock(&channels[channel]->message);
+		// 	if(locked){
+		// 		if(channels[channel]->hadMessage == 0){
+		// 			channels[channel]->item = (void*)malloc(sizeof((void*) msg));
+		// 			channels[channel]->item = msg;
+		// 			msg = NULL;
+		// 			free(msg);
+		// 			channels[channel]->hadMessage = 1;
+		// 			usleep(1000);
+		// 		}else{
+		// 			locked = 0;
+		// 		}
+		// 		pthread_mutex_unlock(&channels[channel]->message);
 				
-			}
-			usleep(1000);
-		}
+		// 	}
+			// usleep(1000);
+		
 		return 0;
 	}
 }
@@ -96,26 +190,43 @@ int ch_recv(int channel, void** dest){
 		puts("Error: Channel number is larger than 7!");
 		return -1;
 	}else{
-		int locked = 0;
-		while(!locked){
+		// usleep(1000);
+		// printf("RECV called on channel %d!!\n", channel);
+		// int locked = 0;
+		// while(!locked)locked = pthread_mutex_trylock(&channels[channel]->recv);
+		pthread_mutex_lock(&channels[channel]->recv);
+		
+		while(channels[channel]->hadMessage == 0)pthread_cond_wait(&channels[channel]->condR,&channels[channel]->recv);
+		// puts("RECV get singal");
+		// pthread_cond_wait(&channels[channel]->condR,&channels[channel]->recv);
+		*dest = (void*)malloc(sizeof((void*) channels[channel]->item));
+		*dest = channels[channel]->item;
+		channels[channel]->item = NULL;
+		free(channels[channel]->item);
+		channels[channel]->hadMessage = 0;
+		pthread_cond_broadcast(&channels[channel]->condS);
+		pthread_mutex_unlock(&channels[channel]->recv);
+		// puts("RECV finished");
+				// int locked = 0;
+		// while(!locked){
 
-			locked = pthread_mutex_trylock(&channels[channel]->recv);
-			if(locked){
-				if(channels[channel]->hadMessage == 1){
-					*dest = (void*)malloc(sizeof((void*) channels[channel]->item));
-					*dest = channels[channel]->item;
-					channels[channel]->item = NULL;
-					free(channels[channel]->item);
-					channels[channel]->hadMessage = 0;
-					usleep(1000);
-				}else{
-					locked = 0;
-				}
-				pthread_mutex_unlock(&channels[channel]->recv);
+		// 	locked = pthread_mutex_trylock(&channels[channel]->recv);
+		// 	if(locked){
+		// 		if(channels[channel]->hadMessage == 1){
+		// 			*dest = (void*)malloc(sizeof((void*) channels[channel]->item));
+		// 			*dest = channels[channel]->item;
+		// 			channels[channel]->item = NULL;
+		// 			free(channels[channel]->item);
+		// 			channels[channel]->hadMessage = 0;
+		// 			usleep(1000);
+		// 		}else{
+		// 			locked = 0;
+		// 		}
+		// 		pthread_mutex_unlock(&channels[channel]->recv);
 
-			}
-			usleep(1000);
-		}
+		// 	}
+			// usleep(1000);
+		
 		return 0;
 	}
 }
@@ -126,8 +237,10 @@ int ch_tryrecv(int channel, void** dest){
 		puts("Error: Channel number is larger than 7!");
 		return -1;
 	}else{
+		puts("Why use me???");
 		if(channels[channel]->hadMessage == 1){
 			if(pthread_mutex_trylock(&channels[channel]->recv) == 0){
+				*dest = (void*)malloc(sizeof((void*) channels[channel]->item));
 				*dest = channels[channel]->item;
 				channels[channel]->item = NULL;
 				free(channels[channel]->item);
